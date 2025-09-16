4fe7fa874691699892f9171c844d52dd983d8e17
Sunner-01 <sunerbarrera228@gmail.com>
2025-09-16 19:28:38 -0400

    Proyecto MVC creado.

---
+++ Pedidos_Yo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pedidos_Yo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

8462d6ce0021ae3022c1ffffe1f1ac78c88cbf90
Sunner-01 <sunerbarrera228@gmail.com>
2025-09-18 21:06:01 -0400

    Creación del controlador de Order mas los CRUDs básicos.

---
+++ Pedidos_Yo/Controllers/OrderController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Pedidos_Yo.Data;
+using Pedidos_Yo.Models;
+
+namespace Pedidos_Yo.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly Pedidos_YoDBContext _context;
+
+        public OrderController(Pedidos_YoDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Order
+        public async Task<IActionResult> Index()
+        {
+            var pedidos_YoDBContext = _context.Orders.Include(o => o.Cliente);
+            return View(await pedidos_YoDBContext.ToListAsync());
+        }
+
+        // GET: Order/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderModel = await _context.Orders
+                .Include(o => o.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (orderModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderModel);
+        }
+
+        // GET: Order/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: Order/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ClienteId,Fecha,Estado,Total")] OrderModel orderModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Users, "Id", "Email", orderModel.ClienteId);
+            return View(orderModel);
+        }
+
+        // GET: Order/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderModel = await _context.Orders.FindAsync(id);
+            if (orderModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Users, "Id", "Email", orderModel.ClienteId);
+            return View(orderModel);
+        }
+
+        // POST: Order/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ClienteId,Fecha,Estado,Total")] OrderModel orderModel)
+        {
+            if (id != orderModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderModelExists(orderModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Users, "Id", "Email", orderModel.ClienteId);
+            return View(orderModel);
+        }
+
+        // GET: Order/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderModel = await _context.Orders
+                .Include(o => o.Cliente)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (orderModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderModel);
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var orderModel = await _context.Orders.FindAsync(id);
+            if (orderModel != null)
+            {
+                _context.Orders.Remove(orderModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderModelExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
